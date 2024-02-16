e81cf6984c5de1520f425d6e9ee1675be9265f2c
Kamal <kamalmiftah01@gmail.com>
2024-02-16 05:34:40 +0100

    solving task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function that sorts an array
+ *			using bubble sort algorithm.
+ *	@array: the array to be sorted.
+ *	@size: the size of the array.
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, idx;
+	int min_int, swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			swapped = 0;
+			if (array[j + 1] < array[j])
+			{
+				min_int = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = min_int;
+
+				swapped = 1;
+			}
+			if (swapped == 1)
+			{
+				for (idx = 0; idx < size; idx++)
+				{
+					printf("%i", array[idx]);
+					if (idx < size - 1)
+						printf(", ");
+				}
+				printf("\n");
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
