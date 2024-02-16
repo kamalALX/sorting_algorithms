6d155a6eb8eec5dce0ab588676c1ee1afbb2ec12
Kamal <kamalmiftah01@gmail.com>
2024-02-16 05:49:12 +0100

    fixing task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 	unsigned int i, j, idx;
 	int min_int, swapped;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
