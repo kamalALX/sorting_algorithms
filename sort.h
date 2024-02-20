e9d967fbc2c49b7caf51e365bbbdcdf891edab95
Amineafilal <amineafilalelalami@gmail.com>
2024-02-20 09:30:04 -0800

    5. Cocktail shaker sort

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
+void cocktail_sort_list(listint_t **list);
 #endif /* _SORTING_*/
 
