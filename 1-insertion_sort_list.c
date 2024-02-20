62dc1b777aa1217d740d7a46bb87ef082e7e2082
Amineafilal <amineafilalelalami@gmail.com>
2024-02-20 07:20:19 -0800

    1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp;
 
-	if (*list == NULL || (*list)->next == NULL || list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	temp = (*list)->next;
