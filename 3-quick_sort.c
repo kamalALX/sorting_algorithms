2093055b569af5f612c94cbbdd976fde989d37ea
Amineafilal <amineafilalelalami@gmail.com>
2024-02-20 06:46:19 -0800

    OK

---
+++ 3-quick_sort.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * quick_sort - Sorts an array of integers using the quicksort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort(array, 0, size - 1, size);
+}
+
+/**
+ * quicksort - Helper function for quick_sort, sorts an array recursively
+ *
+ * @array: The array to be sorted
+ * @left: The left index of the sub-array
+ * @right: The right index of the sub-array
+ * @size: The size of the array
+*/
+void quicksort(int *array, int left, int right, size_t size)
+{
+	int pivot;
+
+	if (left >= right)
+		return;
+	pivot = partition(array, left, right, size);
+	quicksort(array, left, pivot - 1, size);
+	quicksort(array, pivot + 1, right, size);
+}
+
+/**
+ *  partition - Partitions the array for quicksort
+ *
+ * @array: The array to be partitioned
+ * @left: The left index of the sub-array
+ * @right: The right index of the sub-array
+ * @size: The size of the array
+ *
+ * Return: Index of the pivot element after partitioning
+ */
+int partition(int *array, int left, int right, size_t size)
+{
+	int pivot, i, j, temp;
+
+	pivot = array[right];
+	j = left;
+	for (i = left; i < right; i++)
+	{
+		if (array[i] < pivot)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			if (i != j)
+				print_array(array, size);
+			j++;
+		}
+	}
+	temp = array[j];
+	array[j] = array[right];
+	array[right] = temp;
+	if (j != right)
+		print_array(array, size);
+	return (j);
+}
+
--- sort.h
+++ sort.h
@@ -24,5 +24,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t *node_prev, listint_t *node, listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quicksort(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
+#endif /* _SORTING_*/
 
-#endif /* _SORTING_ */
